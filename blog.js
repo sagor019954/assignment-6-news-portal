a416b4aed44ad5e4e2e028e423a334484de4a829
sohel0199 <sohel019954@gmail.com>
2022-10-16 22:25:27 +0600

    news list added

--- blog.js
+++ blog.js
@@ -1,1 +1,26 @@
-console.log('are you ready');
+const loadNewslist = async () => {
+    const newsListUrl = `https://openapi.programming-hero.com/api/news/categories`
+    try {
+        const res = await fetch(newsListUrl);
+        const data = await res.json();
+        displayNewslist(data.data);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+const displayNewslist = (newslists) => {
+    const newsCatagory = newslists.news_category
+    newsCatagory.forEach(news => {
+        const { category_id, category_name } = news;
+        // console.log(category_id, category_name);
+        const newslistcontainer = document.getElementById('newslist-container');
+        const divlist = document.createElement('div');
+        divlist.innerHTML = `
+        <p class="m-3">${category_name}</p>
+        `
+        newslistcontainer.appendChild(divlist);
+    })
+
+}
+loadNewslist();
